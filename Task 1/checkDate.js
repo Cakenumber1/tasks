8bc821918608bcdd9b1cbc8ab6f5787bb7e9f875
Mikhail Nikolaev <cakenumber1@mail.ru>
2022-04-05 19:10:17 +0300

    [task1] implemented task1

--- Task 1/checkDate.js
+++ Task 1/checkDate.js
@@ -1,28 +1,26 @@
 function checkDate(timestamp) {
-    var day = new Date(timestamp * 1000).getDate();
-    var month = new Date(timestamp * 1000).getMonth();
-    var year = new Date(timestamp * 1000).getFullYear();
-    var hour = new Date(timestamp * 1000).getHours();
+  let isSameDate = false;
+  // Так как в примере мы сравниваем только Число, Месяц, Год то создадим переменную options
+  // (Если надо сравнить целиком можно использовать dateOut.getTime() === dateIn.getTime())
+  const options = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  };
+  // Достаточно вызвать по 1 разу функцию Date() для каждого таймстампа
+  const outerDate = new Date(timestamp * 1000).toLocaleDateString(options);
+  // тк нам нужна отдельная строка для pm/am то используем getHours()
+  // а так можно в toLocaleDateString передать { hour12: true } и получить время подпись
+  const curFullDate = new Date();
+  const curDate = curFullDate.toLocaleDateString(options);
+  if (outerDate === curDate) { isSameDate = true; }
 
-    const current_Date = new Date(Date.now());
-    const current_day = current_Date.getDate();
-    const current_month = current_Date.getMonth() + 1;
-    const currentYear = current_Date.getFullYear();
-
-    let isSameDate = false;
-
-    if (year == currentYear) {
-        if (month == current_month) {
-            if (day == current_day) {
-                isSameDate = true;
-            } else {
-                isSameDate = false;
-            }
-        }
-    }
-
-    return {
-        isSameDate: isSameDate,
-        dayPeriod: hour > 11 ? 'pm' : 'am'
-    }
+  return {
+    isSameDate,
+    dayPeriod: curFullDate.getHours() > 12 ? 'pm' : 'am',
+  };
 }
+
+// const unix = Math.round(+new Date() / 1000);
+
+// console.log(checkDate(unix));
